edaad35f28cd01c3110b1bf96bca327ea10681c4
DanKiruja <kirujadanson04@gmail.com>
2024-07-19 13:33:57 +0300

    Add files via upload

---
+++ header and footer.js
@@ -0,0 +1,26 @@
+class Specialheader extends HTMLElement{
+    connectedCallback () {
+        this.innerHTML= `
+         <nav>
+            <a href="index.html">Home</a>
+            <a href="about.html">About</a>
+            <a href="contact.html">Contact</a>
+            <a href="https://kirujadan.wordpress.com/">Blog</a>
+            <a href="gallery.html"></a>
+         </nav>
+
+        `
+    }
+}
+class Specialfooter extends HTMLElement{
+    connectedCallback () {
+        this.innerHTML = `
+            <footer>
+                <p>&copy; 2024 My Web Application</p>
+            </footer>
+        `
+    }
+}
+
+customElements.define('special-header', Specialheader)
+customElements.define('special-footer', Specialfooter)
